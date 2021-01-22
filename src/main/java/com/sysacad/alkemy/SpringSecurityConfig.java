f33e744c9331a277d90aa9f0745d2dd621e4d347
Nestor <nestoramichetti@neuronic.com.ar>
2021-01-22 02:05:38 -0300

    Solucionado el problema para el ingreso con usuario y clave en memoria

--- src/main/java/com/sysacad/alkemy/SpringSecurityConfig.java
+++ src/main/java/com/sysacad/alkemy/SpringSecurityConfig.java
@@ -5,29 +5,32 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 
-	@Bean
+	/*@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
-	}
+	}*/
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/css/**,/js/**").permitAll().anyRequest().authenticated()
+		http.authorizeRequests().antMatchers("/css/**,/js/**").hasRole("USER")
 		.and()
-		.formLogin().permitAll()
-		.and()
-		.logout().permitAll();
+		.formLogin();
+	    http.csrf().disable();
+
 	}
 
 	@Autowired
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("nestor").password("12345").roles("ADMIN");
+		System.out.println("Viene por aca");
+		auth.inMemoryAuthentication().withUser("nestor").password("{noop}12345").roles("USER");
 	}
 	
 }
